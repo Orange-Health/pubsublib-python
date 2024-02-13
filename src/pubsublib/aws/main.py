04de5659887a5da17baf9bda368bca0b02cd4f29
Rajendra Talekar <talekar.r@gmail.com>
2024-02-13 18:15:31 +0530

    Fix variable name in get_large_message method

--- src/pubsublib/aws/main.py
+++ src/pubsublib/aws/main.py
@@ -336,5 +336,5 @@ class AWSPubSubAdapter():
 
         return subscription
 
-    def get_large_message(self,key):
-        return self.cache_adapter.get(redis_key)
+    def get_large_message(self, key):
+        return self.cache_adapter.get(key)
